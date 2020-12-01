516d996260df0d1620981dc292ca51a38fb1c9a2
crush_ <crush_ae@safe-mail.net>
2020-12-01 04:07:40 +0200

    add floating point support and lex operators

--- lexer.c
+++ lexer.c
@@ -7,7 +7,7 @@ typedef enum tokenType {  Number, Plus_Operator, Minus_Operator}tokenType;
 
 typedef struct mathToken {
 	tokenType mathType;
-	int value;
+	double value;
 }mathToken;
 
 mathToken* lexInput(const char* str){
@@ -19,28 +19,67 @@ mathToken* lexInput(const char* str){
 
 	unsigned int currToken = 0;
 	unsigned int currTokenLength = 0;
+	unsigned int currTokenValueSpace = 6;
 	tokenType currTokenType;
 	char* currTokenValueInString;
 
+	//Run through every symbol in the input
 	for(unsigned int i = 0; i <= length; i++) {
-
 
 		if(currTokenLength == 0) {
 
-			//If the first symbol of a token is a number
+			//If the first symbol of a token is an Number
 			if(strchr(digits, str[i]) != NULL) {
-				currTokenValueInString = (char*)malloc(10);
+				//just a random size for the token buffer
+				currTokenValueInString = (char*)malloc(currTokenValueSpace);
 				currTokenType = Number;
+
 				currTokenValueInString[currTokenLength] = str[i];
 				currTokenLength++;
 			}
-		}
 
+			else if(str[i] == '+') {
+				currTokenType = Plus_Operator;
+				//if we now have more tokens than space in the output array, resize
+				if(currToken >= outputSize) {
+					outputSize*=2;
+					output = (mathToken*)realloc(output, sizeof(mathToken) * outputSize);
+				}
+				output[currToken].mathType = currTokenType;
+
+				printf("Створено новий токен [%i +]\n", output[currToken].mathType);
+
+				currToken++;
+				currTokenLength = 0;
+			}
+
+			else if(str[i] == '-') {
+				currTokenType = Minus_Operator;
+				//if we now have more tokens than space in the output array, resize
+				if(currToken >= outputSize) {
+					outputSize*=2;
+					output = (mathToken*)realloc(output, sizeof(mathToken) * outputSize);
+				}
+				output[currToken].mathType = currTokenType;
+
+				printf("Створено новий токен [%i -]\n", output[currToken].mathType);
+
+				currToken++;
+				currTokenLength = 0;
+			}
+
+
+		}
+		//if this isn't the first symbol anymore
 		else{
 
 			if(currTokenType == Number) {
 				//Yet another number, so we add it to the value
-				if(strchr(digits, str[i]) != NULL) {
+				if((strchr(digits, str[i]) != NULL) || (str[i] == '.')) {
+					//if buffer runs out of space
+					if(currTokenLength >= currTokenValueSpace) {
+						currTokenValueInString = (char*)realloc(currTokenValueInString, currTokenValueSpace);
+					}
 					currTokenValueInString[currTokenLength] = str[i];
 					currTokenLength++;
 				}
@@ -48,17 +87,17 @@ mathToken* lexInput(const char* str){
 				//Not a number, so we call it a token
 				else{
 					//if we now have more tokens than space in the output array, resize
-					if(currToken > outputSize) {
+					if(currToken >= outputSize) {
 						outputSize*=2;
 						output = (mathToken*)realloc(output, sizeof(mathToken) * outputSize);
 					}
 					output[currToken].mathType = currTokenType;
 
 					if(currTokenType == Number) {
-						output[currToken].value = atoi(currTokenValueInString);
+						output[currToken].value = atof(currTokenValueInString);
 					}
 
-					printf("Створено новий токен [%i %i]\n", output[currToken].mathType, output[currToken].value);
+					printf("Створено новий токен [%i %lf]\n", output[currToken].mathType, output[currToken].value);
 
 					currToken++;
 					free(currTokenValueInString);
@@ -89,5 +128,5 @@ char* takeInput(){
 
 int main(){
 	//takeInput();
-	lexInput("5 + 5 ");
+	lexInput("5.5 - 7.4123 - 7 + 6 ");
 }
